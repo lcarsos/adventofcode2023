93527b30810172b509806a2582426358cdd6f435
echoteecat <lcarsos@lcarsos.com>
2024-03-27 08:43:38 -0600

    noodling

--- 01/challenge.kt
+++ 01/challenge.kt
@@ -1,4 +1,14 @@
+import java.io.File
+
+fun scooch(calibration: String): Int {
+    val scooch: Char[] = calibration.toCharArray()[0]
+    val left = 0
+    while (scooch[left].toInt()
+
 fun main() {
-    print("hello world")
+    File("test-input.txt").forEachLine { line: String ->
+        print(line.toCharArray()[0])
+        print("\n")
+    }
 }
 
